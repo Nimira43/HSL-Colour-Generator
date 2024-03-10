b1d8aae70f8e35d4e5001144ac95c22646a891b7
Nick Rabone <n.rabone72@aol.com>
2024-03-10 07:43:17 +0000

    ed and some JavaScript variables defined.

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hue = document.getElementById('hue')
+    const saturate = document.getElementById('saturation')
+    const light = document.getElementById('lightness')
+    const hueValue = document.getElementById('hue-value')
+    const saturateValue = document.getElementById('saturation-value')
+    const lightValue = document.getElementById('lightness-value')
+    const copyBtn = document.getElementById('copy-btn')
+
+   hue.addEventListener('input')
+
+})
